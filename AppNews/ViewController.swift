aed9fca30ac93b1aa34303be3be277ec23968fd4
Victor ulises Vazquez arriaga <victorulisesvazquezarriaga@192.168.0.156>
2024-03-20 15:26:28 -0600

    TituloNoticiaLabel white color

--- AppNews/ViewController.swift
+++ AppNews/ViewController.swift
@@ -81,6 +81,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource, SFSafariVi
         celda.tituloNoticiaLabel.text = articuloDeNoticias[indexPath.row].title
         celda.descriptionNoticiaLabel.text = articuloDeNoticias[indexPath.row].description
         celda.backgroundColor = .systemPink
+        celda.tituloNoticiaLabel.textColor = .white
         
         if let urlStringImage = URL(string: articuloDeNoticias[indexPath.row].urlToImage ?? "") {
             let urlImage = URLSession.shared.dataTask(with: urlStringImage) {(data, response, error) in
